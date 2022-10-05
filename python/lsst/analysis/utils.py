4e7011dc4de7580fc1db03068486c7527f4b5d01
Leanne Guy <leanne.guy@lsst.org>
2022-10-05 15:42:25 -0700

    Add utils

---
+++ python/lsst/analysis/utils.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import numpy as np
+
+__all__ = ["nanojanskyToABMagnitude"]
+
+
+def nanojanskyToABMagnitude(flux) -> int:
+    """ Definition from Oke & Gunn (1983). AB magnitude reference flux in nJy
+        (commonly approximated to 3631.0)
+
+    Parameters
+    ----------
+    flux: `float`
+        Flux value in nJy.
+
+    Returns
+    -------
+    ABMag: `float`
+        AB magnitude corresponding to the input flux
+
+    Raises
+    ------
+    Exception
+        Some problem with the conversion (to implement)
+
+    """
+    if flux <= 0:
+        raise ValueError(
+            f"Flux value cannot be negative {flux}"
+        )
+    referenceFlux = 1e23 * np.power(10, (48.6 / -2.5)) * 1e9
+    return -2.5 * np.log10(flux / referenceFlux)
+
